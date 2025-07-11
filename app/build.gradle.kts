e7d71a519d018805a1319aafe056bd9228b0d69a
Vidit <vidit.nakhawa24@gmail.com>
2025-07-11 11:02:15 +0530

    Initial Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,5 @@ dependencies {
     implementation(libs.androidx.work.runtime.ktx)
     implementation(libs.androidx.foundation.layout.android)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.material.icons.extended)
 }
